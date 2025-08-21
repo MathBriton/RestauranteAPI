a512ec151b101af58110f86ee026d08992e09110
MathBriton <mathhbrito@gmail.com>
2025-08-21 16:21:12 -0300

    Subindo Repositories

--- RestauranteAPI/src/RestauranteAPI.Infrastructure/Repositories/OrderRepository.cs
+++ RestauranteAPI/src/RestauranteAPI.Infrastructure/Repositories/OrderRepository.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using RestauranteAPI.Domain.Entities;
+using RestauranteAPI.Domain.Repositories;
+using RestauranteAPI.Infrastructure.Context;
 
-namespace RestauranteAPI.Infrastructure.Repositories
+namespace RestauranteAPI.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
 {
-    internal class OrderRepository
+    private readonly RestauranteContext _context;
+
+    public OrderRepository(RestauranteContext context)
     {
+        _context = context;
+    }
+
+    public async Task AddAsync(Order order)
+    {
+        await _context.Orders.AddAsync(order);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var order = await _context.Orders.FindAsync(id);
+        if (order != null)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await _context.Orders
+                             .Include(o => o.Items)
+                             .Include(o => o.Table)
+                             .ToListAsync();
+    }
+
+    public async Task<Order?> GetByIdAsync(int id)
+    {
+        return await _context.Orders
+                             .Include(o => o.Items)
+                             .Include(o => o.Table)
+                             .FirstOrDefaultAsync(o => o.Id == id);
+    }
+
+    public async Task UpdateAsync(Order order)
+    {
+        _context.Orders.Update(order);
+        await _context.SaveChangesAsync();
     }
 }
--- RestauranteAPI/src/RestauranteAPI.Infrastructure/Repositories/TableRepository.cs
+++ RestauranteAPI/src/RestauranteAPI.Infrastructure/Repositories/TableRepository.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using RestauranteAPI.Domain.Entities;
+using RestauranteAPI.Domain.Repositories;
+using RestauranteAPI.Infrastructure.Context;
 
-namespace RestauranteAPI.Infrastructure.Repositories
+namespace RestauranteAPI.Infrastructure.Repositories;
+
+public class TableRepository : ITableRepository
 {
-    internal class TableRepository
+    private readonly RestauranteContext _context;
+
+    public TableRepository(RestauranteContext context)
     {
+        _context = context;
+    }
+
+    public async Task AddAsync(Table table)
+    {
+        await _context.Tables.AddAsync(table);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var table = await _context.Tables.FindAsync(id);
+        if (table != null)
+        {
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<IEnumerable<Table>> GetAllAsync()
+    {
+        return await _context.Tables.Include(t => t.Orders).ToListAsync();
+    }
+
+    public async Task<Table?> GetByIdAsync(int id)
+    {
+        return await _context.Tables.Include(t => t.Orders)
+                                    .FirstOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task UpdateAsync(Table table)
+    {
+        _context.Tables.Update(table);
+        await _context.SaveChangesAsync();
     }
 }
